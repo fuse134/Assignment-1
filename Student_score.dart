1868de351f08f154c8cce7635528f547b41e28d9
fuse134 <chuaypichai62@gmail.com>
2020-12-22 00:32:45 +0700

    Ass1

---
+++ Student_score.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main() {
+  var nos, score;
+  print("Plaese enter number of student:");
+  nos = stdin.readLineSync();
+  var numberint = int.parse(nos);
+  double total = 0, avg, max = 0, min = 100;
+  for (var i = 0; i < numberint; i++) {
+    print('Enter score student #$i');
+    score = stdin.readLineSync();
+    var scoredouble = double.parse(score);
+    assert(scoredouble == score);
+    total = total + scoredouble;
+    if (scoredouble > max) {
+      max = scoredouble;
+    }
+    if (scoredouble < min) {
+      min = scoredouble;
+    }
+  }
+  print('Total score of $numberint student(s) is $total');
+  avg = total / numberint;
+  print("Average score is $avg");
+  print("Maximum score is $max");
+  print("Minimum score is $min");
+}
